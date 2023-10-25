c7087e7fd28b4982c0ce89a36fe40518c2928ab8
Md Najrul Islam <mdnajrulislam.4738@gmail.com>
2023-10-26 00:16:44 +0600

    added get one method for services and post, get with email filter/query method for bookings

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -27,12 +27,45 @@ async function run() {
     // Send a ping to confirm a successful connection
 
     const serviceCollection = client.db("carDoctor").collection("services");
+    const bookingCollection = client.db("carDoctor").collection("bookings");
 
     // services
     app.get("/services", async (req, res) => {
       const cursor = serviceCollection.find();
       const result = await cursor.toArray();
       //   console.log(result);
+      res.send(result);
+    });
+
+    app.get("/services/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+
+      const options = {
+        // Include only the `title` and `imdb` fields in the returned document
+        projection: { title: 1, price: 1, service_id: 1 },
+      };
+
+      const result = await serviceCollection.findOne(query, options);
+      res.send(result);
+    });
+
+    // booking
+    app.get("/bookings", async (req, res) => {
+      // console.log(req.query?.email);
+      let query = {};
+      if (req.query?.email) {
+        query = { email: req.query.email };
+      }
+      const cursor = bookingCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.post("/bookings", async (req, res) => {
+      const booking = req.body;
+      // console.log(booking);
+      const result = await bookingCollection.insertOne(booking);
       res.send(result);
     });
 
